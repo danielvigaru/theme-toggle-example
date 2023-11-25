0d38e46115c9ffa0cf539dab6769a44a22e0937b
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2023-11-25 13:23:19 +0200

    save prefference to localStorage

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const THEME = {
+    STORAGE_KEY: "theme",
+    LIGHT: "light",
+    DARK: "dark",
+};
+
 const themeToggleBtn = document.getElementById("theme-toggle-btn");
 const themeToggleIcon = document.getElementById("theme-toggle");
 const themeElements = document.getElementsByClassName("theme");
+const getTheme = () => window.localStorage.getItem(THEME.STORAGE_KEY);
 
 function changeToDarkTheme() {
     for (let index = 0; index < themeElements.length; index++) {
@@ -8,6 +15,8 @@ function changeToDarkTheme() {
         element.classList.remove("light-theme");
         element.classList.add("dark-theme");
     }
+
+    window.localStorage.setItem(THEME.STORAGE_KEY, THEME.DARK);
 }
 
 function changeToLightTheme() {
@@ -16,20 +25,36 @@ function changeToLightTheme() {
         element.classList.remove("dark-theme");
         element.classList.add("light-theme");
     }
+
+    window.localStorage.setItem(THEME.STORAGE_KEY, THEME.LIGHT);
 }
 
 themeToggleBtn.addEventListener("click", () => {
-    if (themeToggleIcon.classList.contains("light-theme")) {
+    if (getTheme() === THEME.LIGHT) {
         changeToDarkTheme();
-        return;
+    } else {
+        changeToLightTheme();
     }
-
-    changeToLightTheme();
 });
 
 function setPrefferedTheme() {
-    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-        changeToLightTheme();
+    const theme = getTheme();
+
+    if (theme) {
+        switch (theme) {
+            case THEME.LIGHT:
+                changeToLightTheme();
+                break;
+            case THEME.DARK:
+                changeToDarkTheme();
+                break;
+        }
+    } else {
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            changeToLightTheme();
+        } else {
+            changeToDarkTheme();
+        }
     }
 }
 
